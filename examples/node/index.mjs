291597be8ab2c6bcdfd0ee8f20fff41d4bad3e74
Yuriy Liskov <firsthash@gmail.com>
2024-10-31 04:50:40 +0200

    upd

--- examples/node/index.mjs
+++ examples/node/index.mjs
@@ -65,7 +65,7 @@ const PORT = process.env.PORT || 3000;
 
 // Apply a general rate limit to all requests (1 request per 5 seconds)
 const generalLimiter = rateLimit({
-  windowMs: 3 * 1_000, // 5 seconds
+  windowMs: 5 * 1_000, // 5 seconds
   max: 5, // 1 request per windowMs
   keyGenerator: () => 'global', // Apply limit across all IPs
   message: { error: 'Too many requests, please try again later.' },
